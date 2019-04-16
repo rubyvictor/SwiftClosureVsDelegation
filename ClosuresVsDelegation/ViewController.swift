893581863918757bca2ca64ca852c9057c1d9f79
victor lee ming yew <victor_li@flyscoot.com>
2019-04-16 13:08:58 +0800

    Initial Commit

---
+++ ClosuresVsDelegation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ClosuresVsDelegation
+//
+//  Created by Victor Lee on 16/4/19.
+//  Copyright © 2019 VictorLee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
